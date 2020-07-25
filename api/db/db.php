16439ce99c544bf79f1ae113864036b4ff8bb2d0
Ali <dev.aliakbar@gmail.com>
2020-07-25 12:57:17 +0530

    Added Server Db Cred

--- api/db/db.php
+++ api/db/db.php
@@ -1,4 +1,5 @@
 <?php
+// $conn = mysqli_connect("localhost", "mypreicom_baskin", "baskin", "mypreicom_baskin");
 $conn = mysqli_connect("localhost", "root", "", "baskin_robbins");
 if (!$conn) {
     mysqli_error();
